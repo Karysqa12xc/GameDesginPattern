d182e71d2fbab35e165fffee67e650e6273183ce
Karysqa <85331340+Karysqa12xc@users.noreply.github.com>
2024-02-27 00:43:25 +0700

    Chapter 04: Singleton Pattern

---
+++ Assets/Chapters/Chapter04/Implement a Game Manager with the Singleton/Scripts/GameManger.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace Chapter.Singleton
+{
+    public class GameManger : Singleton<GameManger>
+    {
+        private DateTime _sessionStartTime;
+        private DateTime _sessionEndTime;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // TODO: 
+            // - Load Player save
+            // - If no save, redirect player to registration scene
+            // - Call backend and get daily challenge and rewards
+            _sessionStartTime = DateTime.Now;
+            Debug.Log("Game session start @: " + DateTime.Now);
+        }
+        void OnApplicationQuit()
+        {
+            _sessionEndTime = DateTime.Now;
+            TimeSpan timeDifferent = _sessionEndTime.Subtract(_sessionStartTime);
+
+            Debug.Log("Game session ended @: " + DateTime.Now);
+
+            Debug.Log("Game session lasted: " + timeDifferent);
+        }
+
+        void OnGUI()
+        {
+            if (GUILayout.Button("Next Scene"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Singleton
+* Ưu điểm
+    - Có thể truy cập toàn cầu: Chúng ta có thể sử dụng mẫu Singleton để tạo điểm truy cập toàn cầu vào các tài nguyên hoặc dịch vụ
+    - Kiểm soát đồng thời: Mẫu này có thể được sử dụng để hạn chế quyền truy cập đồng thời vào các tài nguyên được chia sẻ.
+* Nhược điểm:
+    - Khó khăn trong việc kiểm thử đơn vị
+    - Vì dễ tiếp cận nên nó có thể ảnh hưởng đến thói quen lập trình.
+
